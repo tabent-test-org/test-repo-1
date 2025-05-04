2775beb807c0d4480c250fd0896f3eafd8346898
Rohit Kushwaha <97441699+Rohitkushwaha01@users.noreply.github.com>
2025-05-04 12:05:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const AWS_SECRET = "AKIA1234567890FAKEKEY";
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
